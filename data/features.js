dcda1655cddc0e148a74892c8c2c7cee9cbf2825
Alvin Lindstam <alvin.lindstam@gmail.se>
2015-03-07 23:04:25 +0100

    Move data to external file

---
+++ data/features.js
@@ -0,0 +1,27 @@
+var cykelplan_features = {
+  	"object": {
+	    "type": "FeatureCollection",
+	    "features": [
+	        {
+	            "type": "Feature",
+	            "geometry": {
+	                "type": "LineString",
+	                "coordinates": [
+	                    [
+	                        1792356.0411564154,
+	                        8020879.688571336
+	                    ],
+	                    [
+	                        2090766.1995817432,
+	                        8368209.5450991765
+	                    ],
+	                    [
+	                        1640704.9770386256,
+	                        8373101.514909428
+	                    ]
+	                ]
+	            }
+	        }
+	    ]
+	}
+}
--- js/map.js
+++ js/map.js
@@ -2,34 +2,10 @@ $(function(){
 	var raster = new ol.layer.Tile({
 		source: new ol.source.MapQuest({layer: 'sat'})
 	});
-
-	source = new ol.source.GeoJSON(({
-      	object: {
-		    "type": "FeatureCollection",
-		    "features": [
-		        {
-		            "type": "Feature",
-		            "geometry": {
-		                "type": "LineString",
-		                "coordinates": [
-		                    [
-		                        1792356.0411564154,
-		                        8020879.688571336
-		                    ],
-		                    [
-		                        2090766.1995817432,
-		                        8368209.5450991765
-		                    ],
-		                    [
-		                        1640704.9770386256,
-		                        8373101.514909428
-		                    ]
-		                ]
-		            }
-		        }
-		    ]
-		}
-	}));
+	
+	// existing features expected to be contained in cykelplan_features variable
+	// todo: load async on server	
+	source = new ol.source.GeoJSON((cykelplan_features));	
 
 	var layer = new ol.layer.Vector({
 	  	source: source	  	
