c6dc77ea453ec45f78e3c891cc761fe27b92b8fe
Alvin Lindstam <alvin.lindstam@gmail.se>
2015-03-07 23:06:23 +0100

    Add GeoJSON layer

--- js/map.js
+++ js/map.js
@@ -3,62 +3,73 @@ $(function(){
 		source: new ol.source.MapQuest({layer: 'sat'})
 	});
 
+	source = new ol.source.GeoJSON(({
+      	object: {
+		    "type": "FeatureCollection",
+		    "features": [
+		        {
+		            "type": "Feature",
+		            "geometry": {
+		                "type": "LineString",
+		                "coordinates": [
+		                    [
+		                        1792356.0411564154,
+		                        8020879.688571336
+		                    ],
+		                    [
+		                        2090766.1995817432,
+		                        8368209.5450991765
+		                    ],
+		                    [
+		                        1640704.9770386256,
+		                        8373101.514909428
+		                    ]
+		                ]
+		            }
+		        }
+		    ]
+		}
+	}));
+
+	var layer = new ol.layer.Vector({
+	  	source: source	  	
+	});
+
 	var map = new ol.Map({
-	  	layers: [raster],
-	  	target: 'map',
-	  	view: new ol.View({
-	    	center: [-11000000, 4600000],
-	    	zoom: 4
-	  	})
+	  	layers: [raster, layer],
+	  	target: 'map'	  	
 	});
 
-	var featureOverlay = new ol.FeatureOverlay({
-	  style: new ol.style.Style({
-	    fill: new ol.style.Fill({
-	      color: 'rgba(255, 255, 255, 0.2)'
-	    }),
-	    stroke: new ol.style.Stroke({
-	      color: '#ffcc33',
-	      width: 2
-	    }),
-	    image: new ol.style.Circle({
-	      radius: 7,
-	      fill: new ol.style.Fill({
-	        color: '#ffcc33'
-	      })
-	    })
-	  })
-	});
-	featureOverlay.setMap(map);
+	map.getView().fitExtent(source.getExtent(), map.getSize());	
 
-	var draw;
+	var select = new ol.interaction.Select();
+	map.addInteraction(select);
+	
 	var modify = new ol.interaction.Modify({
-	  features: featureOverlay.getFeatures(),	  
-	  deleteCondition: function(event) {
-	    return ol.events.condition.shiftKeyOnly(event) &&
-	        ol.events.condition.singleClick(event);
-	  }
+	  	features: select.getFeatures(),	  
+	  	deleteCondition: function(event) {
+	    	return ol.events.condition.shiftKeyOnly(event) &&
+	        	ol.events.condition.singleClick(event);
+	  	}
 	});
 	map.addInteraction(modify);
 
-	
-	function addInteraction(geom_type) {
-	  map.removeInteraction(draw);
-	  draw = new ol.interaction.Draw({
-	    features: featureOverlay.getFeatures(),
-	    type: geom_type
-	  });
-	  map.addInteraction(draw);
-	  draw.on('drawend', function(){
-	  	setTimeout(function(){
-	  		map.removeInteraction(draw);
-	  	}, 200);	
-	  });
+	var draw;
+	function addDrawInteraction(geom_type) {	  
+	  	draw = new ol.interaction.Draw({
+	    	features: select.getFeatures(),
+	    	type: geom_type
+	  	});
+	  	map.addInteraction(draw);
+		draw.on('drawend', function(ev){
+	  		source.addFeature(ev.feature);	  		  
+	  		setTimeout(function(){
+	  			map.removeInteraction(draw);
+	  		}, 200);	
+	  	});
 	}
 
 	$(".initDrawInteraction").click(function(event){
-		addInteraction($(event.target).data("geomtype"))
-		console.log($(event.target).data("geomtype"));
-	});
-	//addInteraction();
+		addDrawInteraction($(event.target).data("geomtype"))		
+	});	
 });
